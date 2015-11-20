1fe744de6e2d08b91cd6e6fee063d785888956ff
Sergey Poliakov <ps.force@gmail.com>
2015-11-20 17:06:45 +0300

    Добавлено домашнее задание

---
+++ homework/js/gilf.js
@@ -0,0 +1,7 @@
+/**
+ * Created by sergeypoliakov on 20.11.15.
+ */
+var age=window.prompt("Сколько лет вашей бабушке?")
+age++
+console.log (age);
+alert(age);
---
+++ homework/js/xandy.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sergeypoliakov on 20.11.15.
+ */
+    var x = 10, y = 20;
+    var z;
+    z = y;
+    y = x;
+    x = z;
+    console.log (x,y);
+    alert (x);
+    alert (y);
