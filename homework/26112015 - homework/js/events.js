7ba3aa299e727beaa9f61ced134e6bcf85ec1c98
Sergey Poliakov <ps.force@gmail.com>
2015-11-27 16:16:34 +0300

    Добавлены наработки по домашнему заданию

---
+++ homework/26112015 - homework/js/events.js
@@ -0,0 +1,131 @@
+/**
+ * Created by sergeypoliakov on 26.11.15.
+ */
+var event1 = {
+    name: "Achtung Baby",
+    date: "19.11.1991",
+    descrition: "After Rattle and Hum, U2 went away to 'dream it all up again.' Achtung Baby was widely regarded as a sonic and visual reinvention of the band: a step too far in some territories, where Adam's nude image on the sleeve was covered with an appropriately fixed X or clover. At U2's request, Achtung Baby was the first album by a major act to be released in two eco-friendly packages - the jewel box (without the long box attachment) and the cardboard Digi-Trak. The Digi-Trak featured long-box sizes cardboard, which, once its shrink-wrap and two plastic struts are removed, folds around the CD. In Germany, Universal released a 2-CD box set with a copy of both Achtung Baby and Zooropa.",
+    image: "http://media.u2.com/non_secure/images/20090214/discography/albumcover_achtungb/600.jpgu"
+}
+
+var event2 = {
+    name: "Start of U2",
+    date: "25.09.1976",
+    description: "Five young men form a band at Mount Temple High School in Dublin, Ireland. They run through a few names (Feedback, The Hype) and lose a member (Dick Evans) before settling on a name and lineup that stick",
+    image: "http://taihei-taira.c.blog.so-net.ne.jp/_images/blog/_256/taihei-taira/u21.jpg"
+}
+
+var event3 = {
+    name: "Concert in Sarajevo",
+    date: "23.09.1997",
+    description: "U2 play a concert in Sarajevo; they were the first major group to perform there following the Bosnian War. Mullen described the concert as 'an experience I will never forget for the rest of my life, and if I had to spend 20 years in the band just to play that show, and have done that, I think it would have been worthwhile.'",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/U2Sarajevoposter.jpg/220px-U2Sarajevoposter.jpg"
+}
+
+var event4 = {
+    name: "Live Aid Show",
+    date: "13.05.1985",
+    description: "Bono and U2 will make the most of any size platform given them. Not everybody knew that when drummer Larry Mullen Jr. counted off Sunday Bloody Sunday at London's Wembley Stadium, but they sure did 17 minutes later, when the band left the stage. 'They always had ambitions to be the biggest band in the world,' says Rolling Stone contributing editor Anthony DeCurtis, 'and Bono, in particular, understood what a performance in front of an audience of that size could mean.' During a 12-minute rendition of Bad that incorporated bits of Lou Reed and Rolling Stones songs, Bono climbed down from the stage to help security guards extricate three girls from the crush of the crowd. That bit of improvisation cost the band the chance to play its lone U.S. Top 40 hit, Pride (In the Name of Love), but it vaulted the group to a new career stratus.",
+    image: "http://assets.rollingstone.com/assets/images/embedded/u2-liveaid-embed2-600x400-1404995661.jpg"
+}
+
+var event4 = {
+    name: "Live Aid Show",
+    date: "13.05.1985",
+    description: "Bono and U2 will make the most of any size platform given them. Not everybody knew that when drummer Larry Mullen Jr. counted off Sunday Bloody Sunday at London's Wembley Stadium, but they sure did 17 minutes later, when the band left the stage. 'They always had ambitions to be the biggest band in the world,' says Rolling Stone contributing editor Anthony DeCurtis, 'and Bono, in particular, understood what a performance in front of an audience of that size could mean.' During a 12-minute rendition of Bad that incorporated bits of Lou Reed and Rolling Stones songs, Bono climbed down from the stage to help security guards extricate three girls from the crush of the crowd. That bit of improvisation cost the band the chance to play its lone U.S. Top 40 hit, Pride (In the Name of Love), but it vaulted the group to a new career stratus.",
+    image: "http://assets.rollingstone.com/assets/images/embedded/u2-liveaid-embed2-600x400-1404995661.jpg"
+}
+
+var event5 = {
+    name: "The Joshua Tree",
+    date: "09.03.1987",
+    description: "The release of The Joshua Tree saw the band on the cover of Time Magazine billed as 'Rock's Hottest Ticket.' In Sweden, copies of The Joshua Tree were pressed in yellow, pink and red vinyl with sleeves similar to the UK. In Mexico, a limited edition CD was released as part of the Serie Millennium re-issues with a different picture sleeve. In 1999, The Joshua Tree CD was re-issued with the clear band photo that appeared on the original vinyl LP rather than the blurred picture on the initial CD release.",
+    image: "http://cdn.u2.com/non_secure/images/20140418/discography/thejoshuatree_cover/600.jpg"
+}
+
+var event6 = {
+    name: "Every Breaking Wave - first time live",
+    date: "21.08.2010",
+    description: "'Like every breaking wave on the shore, this is as far as I can reach...' A beautiful acoustic number, evidently still a work in progress, with Bono saying to Edge as they jammed towards the end, 'break down the chords to the middle eight again...''No-one has heard that before,' he added, 'Not even us.' We'll all be hearing it again.",
+    image: "http://cdn.u2.com/non_secure/user/images/comments/20100823/033611_dscf0793/full_640.jpg"
+}
+
+var event7 = {
+    name: "Songs of Innocence",
+    date: "09.09.2014",
+    description: "Today in all apple devices over the world...",
+    image: "https://upload.wikimedia.org/wikipedia/ru/9/94/U2_Songs_of_Innocence_cover.jpg"
+}
+
+var event8 = {
+    name: "Stories for Boys at The Late Late Show",
+    date: "15.01.1980",
+    description: "TV Show",
+    image: "https://pbs.twimg.com/media/BfqiK61CMAAKTJf.jpg"
+}
+
+var event9 = {
+    name: "The Unforgettable Fire",
+    date: "01.10.1984",
+    description: "The Unforgettable Fire was U2's first studio collaboration with Brian Eno and Daniel Lanois. There were several significant re-issues including one in October 1996 as part of the Island Master series and one as part of the MFSL Ultradisc series in both the 12 vinyl and 5 CD formats with a slightly longer version of 4th Of July. Other unusual releases from around the world included a Mexican release on the Bertelsmann label as part of the Rockollector series. And, in Australia, the 12 vinyl version was packaged with a bonus stickered 12 of Two Hearts Beat As One (extended)/New Year's Day (edited Version)/Two Hearts Beat As One (4.24).",
+    image: "http://cdn.u2.com/non_secure/images/20140418/discography/unforgettablefire_cover/600.jpg"
+}
+
+var event10 = {
+    name: "One Single",
+    date: "06.03.1992",
+    description: "U2 premiered the song for the first time on February 27, 1992 in Lakeland, Florida while rehearsing for the upcoming Zoo-TV tour. The performance was broadcast that same evening on Top of the Pops. Adam and Larry joined Michael Stipe and Mike Mills of REM for an acoustic performance of One at the MTV Rock & Roll Inaugural Ball on January 20, 1993. They billed themselves as Automatic Baby.",
+    image: "http://cdn.u2.com/non_secure/images/20100325/discography/one_cover/600.jpg"
+}
+
+var events=[event1, event2, event3, event4, event5, event6, event7, event8, event9, event10];
+var min = 0;
+var leng=events.length;
+var max = leng-1;
+var counter = 0;
+var x = Infinity;
+
+fortest=document.getElementById("fortest");
+
+start=confirm('Добрый день! \n Мы представляем вашему вниманию исторический тест \n')
+
+if (start) {
+
+    //рандомизатор
+
+    for (i=0; i<=leng; i++) {
+        x=Math.floor(Math.random()*(max-min+1))+min;
+        var obj=events[x];
+
+        //Показывает картинку и название события
+        fortest.innerHTML='<h2>' + obj.name + '</h2>';
+        fortest.innerHTML+='<img src="' + obj.image + '" alt="' + obj.name + '">';
+        //Задает вопрос пользователю
+        var answer=window.prompt('Когда произошло событие ' + obj.name);
+        if (answer==obj.date) {
+            counter++;
+            alert('Вы ответили правильно!');
+            fortest.innerHTML+='<p class="right.date>' + obj.date + '</p>';
+        }
+        else {
+            alert('Вы ошиблись');
+            fortest.innerHTML+='<p class="wrong.date>' + obj.date + '</p>';
+        }
+        fortest.innerHTML+='<p>'+obj.description+'</p>';
+
+        events.splice(x,1);
+        max--;
+    }
+        alert('Узнать свой результат');
+        fortest.innerHTML='<h2>Поздравляем</h2> <p>Вы ответили правильно на ' + counter +  ' вопросов</p>'
+
+}
+
+//Выход в самом начале
+else {
+    alert("Очень жаль, что вы не прошли этот интереснейший тест. До новых встреч");
+}
+
+//2. Как только пользователь нажал начать показывать ему событие
+//3. 10 событий нужно либо сделать разными объектами, либо одним объектом, чтобы можно было рандомно их показывать
+
